e66170b5148dbed67e85195cfc33536aff759ca5
ROHIT-994 <pasumarthirohith@gmail.com>
2020-02-11 20:18:56 +0530

    3.1

---
+++ EPAM3.1/src/Divide.java
@@ -0,0 +1,7 @@
+
+public class Divide {
+	  public  void division(int a, int b){
+	        System.out.println("division result "+(a/b));
+	    }
+
+}
---
+++ EPAM3.1/src/Subract.java
@@ -0,0 +1,6 @@
+
+public class Subract {
+	public void subtraction(int a, int b) {
+        System.out.println("subtraction result " + (a - b));
+    }
+}
